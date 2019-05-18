5fb95afbcb2cf5d259d6f12839752bbfc5b89b3f
Mariusz Korzekwa <maledorak@gmail.com>
2019-05-18 22:29:59 +0200

    Add polybar multimonitor settings

--- dotfiles/home/config/polybar/launch.sh
+++ dotfiles/home/config/polybar/launch.sh
@@ -6,8 +6,20 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar main_bar -r &
+if type "xrandr"; then
+# based on: https://github.com/polybar/polybar/issues/763
+  primary=$(xrandr --query | grep "primary" | cut -d" " -f1)
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    if [ $m = $primary ]; then
+      tray=right
+    else
+      tray=
+    fi
+    MONITOR=$m TRAY_POSITION=$tray polybar --reload main_bar &
+  done
+else
+  echo "Install xrandr to use multimonitor setup"
+  MONITOR=LVDS-1 TRAY_POSITION=right polybar --reload main_bar &
+fi
 
-
-echo "Bars launched..."
+echo "Polybars launched..."
