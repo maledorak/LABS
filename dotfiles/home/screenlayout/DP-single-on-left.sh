32f0cfe6157df2fba3f8a31892c6cf130b704242
Mariusz Korzekwa <maledorak@gmail.com>
2020-03-22 09:21:39 +0100

    update rc-base

--- dotfiles/home/screenlayout/DP-single-on-left.sh
+++ dotfiles/home/screenlayout/DP-single-on-left.sh
@@ -1,5 +1,2 @@
 #!/bin/sh
-xrandr --output VGA-1 --off  --output DP-1 --off  --output DP-3 --off \
-        --output HDMI-1 --off --output HDMI-2 --off --output HDMI-3 --off
-xrandr --fb 3530x1080 --output LVDS-1 --mode 1600x900 --rotate normal
-xrandr --fb 3530x1080 --output DP-2 --primary --mode 1920x1080 --left-of LVDS-1 --rotate normal
+xrandr --output LVDS-1 --mode 1600x900 --pos 1920x0 --rotate normal --output VGA-1 --off --output HDMI-1 --off --output DP-1 --off --output HDMI-2 --off --output HDMI-3 --off --output DP-2 --off --output DP-3 --primary --mode 1920x1080 --pos 0x0 --rotate normal
