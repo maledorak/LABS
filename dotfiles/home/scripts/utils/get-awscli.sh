de9ef8ea73541ae3ec002479baac81a58729f2cb
Mariusz Korzekwa <maledorak@gmail.com>
2023-04-03 19:30:24 +0200

    add get-awscli script

---
+++ dotfiles/home/scripts/utils/get-awscli.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+AWS_URL="https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip"
+
+if [ -d "/usr/local/aws-cli" ]; then
+    echo "Awscli is already installed"
+
+    INSTALLED_VERSION=$(aws --version | awk '{print $1}' | awk -F/ '{print $2}')
+    LATEST_VERSION=$(curl -s https://api.github.com/repos/aws/aws-cli/tags | jq -r '.[] | .name' | sort -V | tail -n 1)
+
+    if [ "${INSTALLED_VERSION}" == "${LATEST_VERSION}" ]; then
+        echo "Awscli is already the latest version ${INSTALLED_VERSION}"
+        exit 0
+    fi
+    echo "Update awscli ${INSTALLED_VERSION} to ${LATEST_VERSION}}"
+    curl "${AWS_URL}" -o "/tmp/awscliv2.zip"
+    sudo /tmp/aws/install --bin-dir /usr/local/bin --install-dir /usr/local/aws-cli --update
+    rm -rf /tmp/awscliv2.zip /tmp/aws
+    exit 0
+else
+    echo "Install awscli"
+    curl "${AWS_URL}" -o "/tmp/awscliv2.zip"
+    unzip /tmp/awscliv2.zip -d /tmp
+    sudo /tmp/aws/install
+    rm -rf /tmp/awscliv2.zip /tmp/aws
+    exit 0
+fi
